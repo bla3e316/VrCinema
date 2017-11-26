7074f1dbde581810f42fd183a691a4b38e1a2b0e
Aleks <bla3e316@gmail.com>
2017-11-26 13:11:21 +0200

    add button Animation

--- components/scenes/layouts/elements/Button.js
+++ components/scenes/layouts/elements/Button.js
@@ -3,17 +3,56 @@ import {
     Text,
     View,
     VrButton,
-    //Animated
+    Animated
 } from 'react-vr';
 
+import { Easing } from 'react-native';
+
 class Button extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            slideRight: new Animated.Value(1),
+            fadeIn: new Animated.Value(0)
+        };
+    }
+
+
+    componentDidMount() {
+        Animated.sequence([
+            Animated.parallel([
+                Animated.timing(
+                    this.state.slideRight,
+                    {
+                        toValue: 0,
+                        duration: 1500,
+                        easing: Easing.ease
+                    }
+                ),
+                Animated.timing(
+                    this.state.fadeIn,
+                    {
+                        toValue: 1,
+                        duration: 3000,
+                        easing: Easing.ease
+                    }
+                )
+            ])
+        ]).start();
+    }
+
+
     render() {
         return (
-            <View style={{
+            <Animated.View style={{
                 margin: 0.1,
                 height: 0.3,
                 backgroundColor: '#bb13f7',
-                borderRadius: 0.1}}>
+                borderRadius: 0.1,
+                opacity: this.state.fadeIn,
+                transform: [
+                    {translateX: this.state.slideRight}]
+            }}>
                 <VrButton>
                     <Text style={{
                         fontSize: 0.2,
@@ -24,7 +63,7 @@ class Button extends React.Component {
                         {this.props.text}
                     </Text>
                 </VrButton>
-            </View>
+            </Animated.View>
         )
     }
 }
